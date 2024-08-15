45d90e58eba1d18a0feab2aa7b30c23c035b198d
Regis Orate <ott.regisorate@gmail.com>
2024-08-15 18:40:50 +0800

    Deprecate `ApiClient` in favor of `AppApiClient`

--- lib/src/core/api_client.dart
+++ lib/src/core/api_client.dart
@@ -7,18 +7,6 @@ final BaseOptions baseOptions = BaseOptions(
   receiveTimeout: timeoutDuration,
 );
 
-final class ApiClient {
-  /// To be depcrecated
-  Dio get dio {
-    final timeoutDuration = Duration(seconds: 30);
-    final options = BaseOptions(
-      connectTimeout: timeoutDuration,
-      receiveTimeout: timeoutDuration,
-    );
-    return Dio(options);
-  }
-}
-
 final class AuthApiClient {
   /// HTTP Client that the AuthenticationRepository exclusively uses.
   Dio get dio {
--- lib/src/features/hello/hello_repository.dart
+++ lib/src/features/hello/hello_repository.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc_auth/src/core/api_client.dart';
 import 'package:flutter_bloc_auth/src/core/constants.dart';
+import 'package:get_it/get_it.dart';
 
 final class HelloRepository {
-  final Dio dio = ApiClient().dio;
+  final Dio dio = GetIt.I.get<AppApiClient>().dio;
 
   Future<void> hello() async {
     await dio.get(ApiUrl.hello);
